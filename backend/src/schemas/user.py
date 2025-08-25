1c8e5211e45d43ccc6fa5eb6c132637bf8404b69
Kostiantyn L <feed7362@gmail.com>
2025-08-25 18:47:02 +0300

    add user model enhancements: include is_active and is_verified fields in UserBase; implement UserUpdate model for user modifications

--- backend/src/schemas/user.py
+++ backend/src/schemas/user.py
@@ -1,5 +1,4 @@
 from pydantic import BaseModel, EmailStr
-from typing import Optional
 from uuid import UUID
 from datetime import datetime
 
@@ -7,17 +6,28 @@ from datetime import datetime
 class UserBase(BaseModel):
     username: str
     email: EmailStr
+    is_active: bool
+    is_verified: bool
 
 
 class UserCreate(UserBase):
     password: str  # plain, will be hashed before save
+    is_active: bool = True
+    is_verified: bool = False
 
 
 class UserRead(UserBase):
     id: UUID
-    is_active: bool
-    is_verified: bool
     registered_at: datetime
 
     class Config:
         from_attributes = True
+
+
+class UserUpdate(UserBase):
+    id: UUID
+    email: EmailStr
+    username: str
+    role_id: UUID
+    is_active: bool
+    is_verified: bool
