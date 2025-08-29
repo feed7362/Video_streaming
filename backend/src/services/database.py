67bf91f1d1655663d307945aa261379f90191097
Kostiantyn L <feed7362@gmail.com>
2025-08-29 17:48:40 +0300

    Refactor database to lazy load

--- backend/src/services/database.py
+++ backend/src/services/database.py
@@ -2,29 +2,36 @@ from contextlib import asynccontextmanager
 from typing import AsyncGenerator
 
 from sqlalchemy import MetaData
-from sqlalchemy.ext.asyncio import AsyncSession, async_sessionmaker, create_async_engine
-from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.ext.asyncio import (
+    AsyncEngine,
+    AsyncSession,
+    async_sessionmaker,
+    create_async_engine,
+)
+from sqlalchemy.orm import declarative_base
 
 from ..config import get_database_settings
 
-settings = get_database_settings()
-
-DATABASE_URL = (
-    f"postgresql+asyncpg://{settings.POSTGRES_USER}:{settings.POSTGRES_PASSWORD}"
-    f"@{settings.POSTGRES_HOST}:{settings.POSTGRES_PORT}/{settings.POSTGRES_DB}"
-)
-
-async_engine = create_async_engine(DATABASE_URL, echo=True)
 metadata = MetaData()
-
 Base = declarative_base(metadata=metadata)
 
-async_session_maker = async_sessionmaker(
-    async_engine, expire_on_commit=False, class_=AsyncSession
-)
+
+async def get_engine() -> AsyncEngine:
+    settings = get_database_settings()
+    database_url = (
+        f"postgresql+asyncpg://{settings.POSTGRES_USER}:{settings.POSTGRES_PASSWORD}"
+        f"@{settings.POSTGRES_HOST}:{settings.POSTGRES_PORT}/{settings.POSTGRES_DB}"
+    )
+    return create_async_engine(database_url, echo=True)
+
+
+async def get_async_session_maker() -> async_sessionmaker:
+    engine = await get_engine()
+    return async_sessionmaker(engine, expire_on_commit=False, class_=AsyncSession)
 
 
 @asynccontextmanager
 async def get_async_session() -> AsyncGenerator[AsyncSession, None]:
-    async with async_session_maker() as session:
+    session_maker = await get_async_session_maker()
+    async with session_maker() as session:
         yield session
